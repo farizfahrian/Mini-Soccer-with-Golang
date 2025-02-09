e44982518591dd2a564fe919376de408c879ac66
Snawlaye <farizfahrian53@gmail.com>
2025-02-09 21:42:24 +0800

    fix error on user models

--- domain/models/user.go
+++ domain/models/user.go
@@ -17,5 +17,5 @@ type User struct {
 	RoleID      uint      `gorm:"type:uint;not null"`
 	CreatedAt   *time.Time
 	UpdateAt    *time.Time
-	Role        Role `gorm:"foreignKey:role_id;references:id;constraint:OnUpdate"`
+	Role        Role `gorm:"foreignKey:role_id;references:id;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 }
